7fa01e144effda244cf63d8fe00f7d0d425b2819
RishabhKathait <100272464+RishabhKathait@users.noreply.github.com>
2024-01-31 12:33:27 +0530

    Add files via upload

---
+++ pratical.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let check_value = 40;
+    if check_value > 100 {
+        println!("The decision returns true !");
+    } else {
+        println!("The decision returns false !");    }
+}
